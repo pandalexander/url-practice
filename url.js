24374fbea13321f628d143eb27286748eceb8464
Alex Seeley <alexseeley134@gmail.com>
2024-10-10 06:42:02 -0600

    Practice input vs base url constructor

--- url.js
+++ url.js
@@ -4,4 +4,8 @@ const myUrl = new URL("https://myexample.com");
 
 myUrl.pathname = "/a/path/name";
 
-console.log(myUrl.href);
+// console.log(myUrl.href);
+
+const newUrl = new URL("/print/me", "https://secondexample.com");
+
+console.log(newUrl.href);
